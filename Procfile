d3a91e0265bb7930df634a599cb18986291c079a
Josue Santos <git@joss.cc>
2024-04-09 12:18:50 -0300

    start project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dclojure.main.report=stderr -cp target/uberjar/cumbuca.jar clojure.main -m cumbuca.core
---
+++ env/prod/clj/cumbuca/env.clj
@@ -0,0 +1,11 @@
+(ns cumbuca.env
+  (:require [clojure.tools.logging :as log]))
+
+(def defaults
+  {:init
+   (fn []
+     (log/info "\n-=[cumbuca started successfully]=-"))
+   :stop
+   (fn []
+     (log/info "\n-=[cumbuca has shut down successfully]=-"))
+   :middleware identity})
